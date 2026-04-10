ade93806986520607eb7d18112bd2ffb253664b4
bappy958 <itznobita958@gmail.com>
2026-04-11 03:41:11 +0600

    after remove cros issue

--- core/settings.py
+++ core/settings.py
@@ -84,6 +84,21 @@ DATABASES = {
 }
 
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Dhaka'
 USE_I18N = True
